b165d56656f23f90a132a7446e16c30bfc0874fe
Peter Cooper <git@peterc.org>
2026-03-06 13:58:31 +0000

    Consolidate dimension tests into data-driven methods

--- test/test_dimensions.rb
+++ test/test_dimensions.rb
@@ -5,137 +5,25 @@ require_relative "test_helper"
 class TestDimensions < Minitest::Test
   include TestHelper
 
-  # --- Exact multiples of 8 (baseline, should always work) ---
-
-  def test_8x8
-    assert_round_trips(8, 8)
+  def test_color_dimensions
+    [
+      [8, 8], [16, 16],             # exact multiples of 8
+      [7, 7], [9, 9], [15, 15],    # non-multiple-of-8
+      [17, 17], [13, 21], [21, 13],
+      [24, 24], [31, 31], [33, 33], # non-multiple-of-16 (4:2:0 subsampling)
+      [1, 1], [2, 2], [3, 5],      # very small
+      [1, 8], [8, 1], [1, 16], [16, 1],
+      [4, 64], [64, 4],            # asymmetric
+      [100, 8], [8, 100],
+      [37, 41], [97, 53],          # odd primes
+    ].each { |w, h| assert_round_trips(w, h) }
   end
 
-  def test_16x16
-    assert_round_trips(16, 16)
+  def test_grayscale_dimensions
+    [[1, 1], [3, 5], [7, 7], [15, 15], [33, 33]].each do |w, h|
+      assert_round_trips(w, h, grayscale: true)
+    end
   end
-
-  # --- Non-multiple-of-8 dimensions ---
-
-  def test_7x7
-    assert_round_trips(7, 7)
-  end
-
-  def test_9x9
-    assert_round_trips(9, 9)
-  end
-
-  def test_15x15
-    assert_round_trips(15, 15)
-  end
-
-  def test_17x17
-    assert_round_trips(17, 17)
-  end
-
-  def test_13x21
-    assert_round_trips(13, 21)
-  end
-
-  def test_21x13
-    assert_round_trips(21, 13)
-  end
-
-  # --- Non-multiple-of-16 (matters for 4:2:0 chroma subsampling) ---
-
-  def test_24x24_color
-    assert_round_trips(24, 24)
-  end
-
-  def test_31x31_color
-    assert_round_trips(31, 31)
-  end
-
-  def test_33x33_color
-    assert_round_trips(33, 33)
-  end
-
-  # --- Very small images ---
-
-  def test_1x1
-    assert_round_trips(1, 1)
-  end
-
-  def test_1x1_grayscale
-    assert_round_trips(1, 1, grayscale: true)
-  end
-
-  def test_2x2
-    assert_round_trips(2, 2)
-  end
-
-  def test_1x8
-    assert_round_trips(1, 8)
-  end
-
-  def test_8x1
-    assert_round_trips(8, 1)
-  end
-
-  def test_1x16
-    assert_round_trips(1, 16)
-  end
-
-  def test_16x1
-    assert_round_trips(16, 1)
-  end
-
-  def test_3x5
-    assert_round_trips(3, 5)
-  end
-
-  # --- Asymmetric dimensions ---
-
-  def test_wide_4x64
-    assert_round_trips(4, 64)
-  end
-
-  def test_tall_64x4
-    assert_round_trips(64, 4)
-  end
-
-  def test_wide_100x8
-    assert_round_trips(100, 8)
-  end
-
-  def test_tall_8x100
-    assert_round_trips(8, 100)
-  end
-
-  # --- Odd primes (worst case for block alignment) ---
-
-  def test_37x41
-    assert_round_trips(37, 41)
-  end
-
-  def test_97x53
-    assert_round_trips(97, 53)
-  end
-
-  # --- Grayscale variants (no subsampling, only 8x8 block alignment matters) ---
-
-  def test_7x7_grayscale
-    assert_round_trips(7, 7, grayscale: true)
-  end
-
-  def test_15x15_grayscale
-    assert_round_trips(15, 15, grayscale: true)
-  end
-
-  def test_33x33_grayscale
-    assert_round_trips(33, 33, grayscale: true)
-  end
-
-  def test_3x5_grayscale
-    assert_round_trips(3, 5, grayscale: true)
-  end
-
-  # --- Pixel accuracy for non-aligned dimensions ---
 
   def test_pixel_accuracy_non_aligned_color
     w, h = 19, 23
@@ -143,7 +31,6 @@ class TestDimensions < Minitest::Test
     data = PureJPEG.encode(source, quality: 95).to_bytes
     image = PureJPEG.read(data)
 
-    # Check corners and center
     [[0, 0], [w - 1, 0], [0, h - 1], [w - 1, h - 1], [w / 2, h / 2]].each do |x, y|
       pixel = image[x, y]
       assert_in_delta 200, pixel.r, 30, "Red at (#{x},#{y})"
@@ -165,12 +52,9 @@ class TestDimensions < Minitest::Test
     end
   end
 
-  # --- Edge pixels are within image bounds (no padding leakage) ---
-
   def test_no_padding_leakage_color
     w, h = 10, 10
     source = PureJPEG::Source::RawSource.new(w, h) do |x, y|
-      # Bright interior, distinct border
       if x == 0 || y == 0 || x == w - 1 || y == h - 1
         [255, 0, 0]
       else
@@ -184,7 +68,6 @@ class TestDimensions < Minitest::Test
     assert_equal w, image.width
     assert_equal h, image.height
 
-    # All pixels should be accessible without error
     h.times do |y|
       w.times do |x|
         assert_valid_pixel(image[x, y])
@@ -211,7 +94,6 @@ class TestDimensions < Minitest::Test
     assert_equal width, image.width, "#{width}x#{height}: decoded width mismatch"
     assert_equal height, image.height, "#{width}x#{height}: decoded height mismatch"
 
-    # Verify all pixels are accessible and valid
     assert_valid_pixel(image[0, 0])
     assert_valid_pixel(image[width - 1, height - 1])
 
