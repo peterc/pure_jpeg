3a7648e0c74550fc59d34454caf842e21f9a3a40
Peter Cooper <git@peterc.org>
2026-03-06 10:11:38 +0000

    Support PNG inputs in kodak example script

--- examples/kodak.rb
+++ examples/kodak.rb
@@ -2,15 +2,21 @@
 require_relative "../lib/pure_jpeg"
 
 if ARGV.length < 1
-  $stderr.puts "Usage: #{$0} INPUT.jpg [OUTPUT.jpg] [quality]"
+  $stderr.puts "Usage: #{$0} INPUT.(jpg|png) [OUTPUT.jpg] [quality]"
   exit 1
 end
 
 input = ARGV[0]
-output = ARGV[1] || input.sub(/(\.\w+)$/, '_kodak\1')
+output = ARGV[1] || input.sub(/(\.\w+)$/, '_kodak.jpg')
 quality = (ARGV[2] || 20).to_i
 
-image = PureJPEG.read(input)
-PureJPEG.encode(image, quality: quality, scramble_quantization: true).write(output)
+if input.downcase.end_with?(".png")
+  require "chunky_png"
+  source = PureJPEG::Source::ChunkyPNGSource.new(ChunkyPNG::Image.from_file(input))
+else
+  source = PureJPEG.read(input)
+end
+
+PureJPEG.encode(source, quality: quality, scramble_quantization: true).write(output)
 
 puts "#{input} -> #{output} (#{File.size(output)} bytes, q#{quality}, scrambled quantization)"
