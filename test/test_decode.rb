1f034b628b9db02de838d077c4d76eabdd4bb95d
Peter Cooper <git@peterc.org>
2026-03-06 11:16:09 +0000

    Add tests for decoding external JPEG files
    Tests decode of examples/a.jpg (produced by an external tool) and
    a full decode-then-re-encode round trip, catching encoder/decoder
    bugs that would cancel out in self-round-trip tests.

--- test/test_decode.rb
+++ test/test_decode.rb
@@ -80,4 +80,42 @@ class TestDecode < Minitest::Test
     assert_in_delta 0, pixel.g, 30, "Green channel should be close to 0"
     assert_in_delta 0, pixel.b, 30, "Blue channel should be close to 0"
   end
+
+  def test_decode_external_jpeg
+    path = File.expand_path("../examples/a.jpg", __dir__)
+    image = PureJPEG.read(path)
+
+    assert_equal 1024, image.width
+    assert_equal 1024, image.height
+
+    # Spot-check pixels at corners and center
+    [[0, 0], [1023, 0], [0, 1023], [1023, 1023], [512, 512]].each do |x, y|
+      assert_valid_pixel(image[x, y])
+    end
+
+    # Verify all pixels are valid
+    count = 0
+    image.each_pixel do |_x, _y, pixel|
+      assert_includes 0..255, pixel.r
+      assert_includes 0..255, pixel.g
+      assert_includes 0..255, pixel.b
+      count += 1
+    end
+    assert_equal 1024 * 1024, count
+  end
+
+  def test_re_encode_external_jpeg
+    path = File.expand_path("../examples/a.jpg", __dir__)
+    image = PureJPEG.read(path)
+
+    re_encoded = PureJPEG.encode(image, quality: 75).to_bytes
+    assert re_encoded.start_with?("\xFF\xD8".b)
+    assert re_encoded.end_with?("\xFF\xD9".b)
+
+    # Decode the re-encoded output to verify it survives a full round trip
+    image2 = PureJPEG.read(re_encoded)
+    assert_equal 1024, image2.width
+    assert_equal 1024, image2.height
+    assert_valid_pixel(image2[512, 512])
+  end
 end
